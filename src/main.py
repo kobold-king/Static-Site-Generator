edee14fb7daa8b38939fa50dd1be94021d403676
kobold-king <nick.ragona@gmail.com>
2025-04-28 21:17:16 -0600

    adding to github

--- src/main.py
+++ src/main.py
@@ -5,11 +5,17 @@ from static_to_public import static_to_public
 from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+#dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+        print(f"Base path set to: {basepath}")
+    else:
+        basepath = "/"
 
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
@@ -19,7 +25,7 @@ def main():
     static_to_public(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
     #generate_pages_recursive(
         #os.path.join(dir_path_content, "index.md"),
         #template_path,
